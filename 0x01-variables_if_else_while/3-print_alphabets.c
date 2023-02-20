03647a1e159e38c825a364ca052d83ccf12232bb
Dianerex <dianerex@gmail.com>
2023-02-20 10:53:04 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alphABET
+ *
+ * Return; Always 0(successful)
+ */
+int main(void)
+{
+	char ltu;
+
+	for (ltu = 'a'; ltu <= 'z'; ltu++)
+		putchar(ltu);
+	for (ltu = 'A'; ltu <= 'Z'; ltu++)
+		putchar(ltu);
+			putchar('\n');
+			return (0);
+}
