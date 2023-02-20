1b63d8f5fde34f8f0e5e1089709430054b0d8898
Dianerex <dianerex@gmail.com>
2023-02-20 14:30:39 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
