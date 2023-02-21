5743743b7a1d2d8ae3c45995f257cf3b092dc29f
Dianerex <dianerex@gmail.com>
2023-02-20 17:14:53 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints possible combinations of single-digit num
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
