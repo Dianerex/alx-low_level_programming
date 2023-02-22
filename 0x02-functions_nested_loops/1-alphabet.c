0fc66c9b923b6ee888ee6ed71a428273cd882be5
Dianerex <dianerex@gmail.com>
2023-02-21 18:28:15 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -  A program that prints the alphabet, in lowercase
+ *
+ * Return: Always 0;
+ */
+int main(void)
+{
+	char (lc);
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(lc);
+	{
+		_putchar('\n');
+	}
+	return (0);
+}
