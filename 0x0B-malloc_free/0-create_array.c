2fa28f88cc144039a70bc68e42a9dcc1c0596a5a
Dianerex <dianerex@gmail.com>
2023-03-15 07:46:31 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *array = malloc(size);
+
 if (size == 0 || array == 0)
 return (NULL);
 while (size--)
