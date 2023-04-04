7fe9a92ea2c3f0033c9881e5a88cfd3da65c3826
Dianerex <dianerex@gmail.com>
2023-04-04 15:50:58 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,13 +3,13 @@
 
 /**
 * print_listint - Prints all the elements of a listint_t list.
-* @h: A pointer to the head of the list_t list.
+* @h: linked list of type listint_t to print.
 *
 * Return: The number of nodes in the list_t list.
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+	size_t num = 0;
 
 while (h != NULL)
 {
